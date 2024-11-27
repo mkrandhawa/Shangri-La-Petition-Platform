957e344f7f0f4196928ffceeec85d030b820b0c7
mkrandhawa <mkrandhawa71@gmail.com>
2024-11-27 23:01:01 +0000

    Update

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
