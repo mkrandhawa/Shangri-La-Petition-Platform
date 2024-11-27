272c0e2e13b05e2585cc2b1847eea5e34dccac74
mkrandhawa <mkrandhawa71@gmail.com>
2024-11-27 22:59:42 +0000

    Added lgout route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post('/auth/signup', userController.register);
 router.post('/auth/login', userController.login);
+router.post('/auth/logout', userController.logout);
 
 
 module.exports = router;
