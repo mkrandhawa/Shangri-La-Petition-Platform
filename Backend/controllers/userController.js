433ed536a040a38b760af67a588bad8d5ca96856
mkrandhawa <mkrandhawa71@gmail.com>
2024-11-27 22:59:15 +0000

    Added bioids checks and logout function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const data = require('../public/data/bioId');
 
 //Generate JSON Taken
 
@@ -11,6 +12,7 @@ const generateJsonToken = (userId) => {
 
 };
 
+console.log(data)
 //Register User
 
 exports.register = async(req, res, next)=>{
@@ -18,6 +20,24 @@ exports.register = async(req, res, next)=>{
     const username = req.body.email.split('@')[0];
 
     const {email, fullName, password, confirmPassword, bioId, role } = req.body;
+
+    const isBioId = await User.find({bioId});
+    
+
+
+    if(!isBioId.length==0){
+        res.status(400).json({
+            status: 'Fail',
+            message: 'This is not your Bio ID!'
+        });
+        return;
+    }else if ( !data.includes(bioId)){
+        res.status(400).json({
+            status: 'Fail',
+            message: 'Invalid Bio ID! Check and try again'
+        });
+        return;
+    }
 
     const user = await User.create({
         email,
@@ -92,4 +112,20 @@ exports.login = async(req, res, next)=>{
         message: 'You are logged in!'
     });
 
+}
+
+//Logout User
+
+exports.logout = async(req, res, next)=>{
+
+    res.cookie('jsonToken', '',{
+        httpOnly: true,
+        secure: process.env.NODE_ENV==='production',
+        expiresIn: new Date(0)
+    });
+
+    res.status(200).json({
+        status: 'Success',
+        message: 'You have been logged out'
+    })
 }
