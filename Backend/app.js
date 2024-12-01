cf9886c5ccc26762d7b9bbea1258aa17d97a2f75
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-01 19:57:56 +0000

    Added cookie parser

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/userRoutes');
+const petitionRoutes = require('./routes/petitionRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use('/api/user/', userRoutes);
+app.use('/api/petitions', petitionRoutes);
 
 module.exports = app;
