38108fd28b83f25aa05df965f3cb8bcc46b1f017
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-06 21:36:29 +0000

    Added sign petition route

--- Backend/routes/petitionRoutes.js
+++ Backend/routes/petitionRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 
 router.get('/', petitionController.getAllPetitions);
 router.post('/addPetition', userController.protect, petitionController.addPetition);
+router.patch('/:petitionId/sign', userController.protect, petitionController.signPetition);
 
 module.exports=router;
