983a2675229c382e2b64230efac4e7243fcc98e5
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-06 21:36:12 +0000

    Added sign function

--- Backend/controllers/petitionController.js
+++ Backend/controllers/petitionController.js
@@ -1,3 +1,4 @@
+const Petition = require('../models/petitionModel');
 const Petiton = require('../models/petitionModel');
 const User = require('../models/userModel');
 
@@ -55,3 +56,65 @@ exports.addPetition = async(req, res, next)=>{
     });
 }
 
+// Sign a Petition --> /api/petitions/:petitionId/sign
+
+exports.signPetition = async(req, res, next)=>{
+
+    const userId = req.user.id;
+
+    const petitionId = req.params.petitionId;
+
+    // If there is not petition ID or if the user is no Logged in --> ERROR
+
+    if(!userId || !petitionId){
+
+        next(res.status(400).json({
+            status: 'Fail',
+            message: 'Please login/provide valid petition ID'
+        }));
+    }
+
+    const petition = await Petiton.findById(petitionId);
+
+    const user = await  User.findById(userId);
+
+    const petitionSigned = user.signedPetitions.includes(petitionId);
+
+    // If the petition has not been signed before and logged user has role 'user' --> Continue Signing
+
+    if(!petitionSigned && petition && user.role === 'user'){
+
+        //Update the references to the petition document
+
+        const updated = await User.updateOne(
+            {_id: userId},
+            {$push:{
+                signedPetitions: petitionId
+            }}, 
+            {new: true}
+        );
+
+        // Increase the signs for the petition
+
+        await Petition.updateOne(
+            {_id: petitionId}, 
+            {$inc:{
+                countSigns: 1
+            }}
+        );
+
+        res.status(200).json({
+            staus: 'Success',
+            message: 'You have signed successfully',
+            data: updated
+        });
+    
+    }else{
+        next(res.status(400).json({
+            status: 'Fail',
+            message: 'You cannot sign the same petition twice'
+        }))
+    }
+
+
+}
