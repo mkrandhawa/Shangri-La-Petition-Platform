91ea6843e4972ce51b940d90d2807696a1acca5f
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-06 22:18:28 +0000

    Added commitee route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const petitionController = require('../controllers/petitionController');
 const router = express.Router();
 
 router.post('/auth/signup', userController.register);
 router.post('/auth/login', userController.login);
 router.post('/auth/logout', userController.logout);
+router.patch('/commitee/:petitionId/threshold', userController.protect, petitionController.setThreshold);
+
 
 
 module.exports = router;
