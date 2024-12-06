d8e48cf7bd6a7c1b96f18295beea5540bc0611b3
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-06 22:18:38 +0000

    Updated base route

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/api/user/', userRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/petitions', petitionRoutes);
 
 module.exports = app;
