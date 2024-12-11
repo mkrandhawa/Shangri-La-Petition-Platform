0a14c8271f06e47afc939c38f195fbf01bf9f310
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-11 19:38:37 +0000

    Added status route

--- Backend/routes/petitionRoutes.js
+++ Backend/routes/petitionRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const router = express.Router();
 const petitionController = require('../controllers/petitionController');
 const userController = require('../controllers/userController');
 
+const router = express.Router();
+
+router.get('/:status?', petitionController.queryOpenPetitions);
+    
 router.get('/', petitionController.getAllPetitions);
+
 router.post('/addPetition', userController.protect, petitionController.addPetition);
+
 router.patch('/:petitionId/sign', userController.protect, petitionController.signPetition);
+
+
 
 module.exports=router;
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const petitionController = require('../controllers/petitionController');
+
 const router = express.Router();
 
 router.post('/auth/signup', userController.register);
+
 router.post('/auth/admin/signUp', userController.registerAdmin);
+
 router.post('/auth/login', userController.login);
+
 router.post('/auth/logout', userController.logout);
+
 router.patch('/commitee/:petitionId/threshold', userController.protect, petitionController.setThreshold);
+
+router.patch('/admin/:petitionId/respond', userController.protect, petitionController.respondPetition);
 
 
 
